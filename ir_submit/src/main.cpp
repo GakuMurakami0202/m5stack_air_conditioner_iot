c63b29a438d979f4322cd7f14d579087a5ab0ec8
{GakuMurakami0202} <{2gakugaku2@gmail.com}>
2024-08-12 13:58:28 +0900

    赤外線送信のピン番号を26に変更

--- ir_submit/src/main.cpp
+++ ir_submit/src/main.cpp
@@ -1,13 +1,15 @@
 #include <M5Core2.h>
 #include <IRsend.h>
 #include <IRremoteESP8266.h>
+#include <IRutils.h>
 
-// 赤外線送信ピン
-const uint16_t kIrLedPin = 9;  // M5Stack Core2の場合、通常は9ピンを使用
+// 赤外線送信ピンを26に設定
+const uint16_t kIrLedPin = 26; 
+const uint16_t kFrequency = 38000; // 38kHzの周波数
 
-IRsend irsend(kIrLedPin);
+IRsend irsend(kIrLedPin); // 赤外線送信機の初期化
 
-// 関数のプロトタイプ宣言
+// 関数プロトタイプの宣言
 void sendCoolingOn();
 void sendAcOff();
 
@@ -27,38 +29,32 @@ void loop() {
   // ボタンAを押すと冷房をオンにする
   if (M5.BtnA.wasPressed()) {
     M5.Lcd.println("Cooling ON");
-    sendCoolingOn();
+    for (int i = 0; i < 5; i++) { // 信号を5回送信
+      sendCoolingOn();
+      delay(500); // 信号の間に遅延を入れる
+    }
   }
 
   // ボタンBを押すとエアコンをオフにする
   if (M5.BtnB.wasPressed()) {
     M5.Lcd.println("AC Off");
-    sendAcOff();
+    for (int i = 0; i < 5; i++) { // 信号を5回送信
+      sendAcOff();
+      delay(500); // 信号の間に遅延を入れる
+    }
   }
-  
+
   delay(100);
 }
 
+// 冷房をオンにする信号を送信する関数
 void sendCoolingOn() {
-  uint64_t coolingOnSignal = 0x80080002FDFF;
-  uint64_t unknownSignal1 = 0x30324473;
-  uint64_t unknownSignal2 = 0xDADB4F40;
-
-  irsend.sendPanasonic(coolingOnSignal, 48);
-  delay(100);  // 遅延を少し長くしてみる
-  irsend.sendNEC(unknownSignal1, 50);  
-  delay(100);
-  irsend.sendNEC(unknownSignal2, 50);
+  uint16_t coolingOnSignal[] = {0xCC00, 0xFFBF, 0x4000, 0x1001, 0x0110, 0x0040, 0xBFFF, 0x00CC, 0x3392, 0x6D13, 0xEC74, 0x8B00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0x25DA, 0xD12E, 0x00FF, 0x00FF};
+  irsend.sendRaw(coolingOnSignal, sizeof(coolingOnSignal) / sizeof(coolingOnSignal[0]), kFrequency);
 }
 
+// エアコンをオフにする信号を送信する関数
 void sendAcOff() {
-  uint64_t acOffSignal = 0x80080002FDFF;
-  uint64_t unknownSignal1 = 0x930060D7;
-  uint64_t unknownSignal2 = 0xEE0896F6;
-
-  irsend.sendPanasonic(acOffSignal, 48);
-  delay(300);  // 遅延を少し長くしてみる
-  irsend.sendNEC(unknownSignal1, 50);  
-  delay(300);
-  irsend.sendNEC(unknownSignal2, 50);
+  uint16_t acOffSignal[] = {0xCC00, 0xFFBF, 0x4000, 0x1001, 0x0110, 0x0040, 0xBFFF, 0x00CC, 0x3392, 0x6D13, 0xEC74, 0x8B00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0xFF00, 0x25DA, 0xC13E, 0x00FF, 0x00FF};
+  irsend.sendRaw(acOffSignal, sizeof(acOffSignal) / sizeof(acOffSignal[0]), kFrequency);
 }
